d3e5b333f6abc25d2b7833b6d05b58f197f52c63
Khristianna-Vov <vkhristianna@gmail.com>
2024-07-17 03:47:20 +0500

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,8 @@
+name = 'Христианна'
+age = 56
+new_age = 156
+is_student = (name == 'Христианна')
+print('Имя -',name)
+print('Возраст - ',age)
+print('Будет когда-то - ',new_age)
+print('Я, студент',is_student)
