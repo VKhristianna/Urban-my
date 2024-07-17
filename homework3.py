1696566b8718c9eb7c5f12bd261e481626708276
Khristianna-Vov <vkhristianna@gmail.com>
2024-07-17 13:00:46 +0500

    homework3.py
    Исправлено

--- homework3.py
+++ homework3.py
@@ -1,8 +1,8 @@
 name = 'Христианна'
 age = 56
-new_age = 156
+new_age = age + 5
 is_student = (name == 'Христианна')
 print('Имя -',name)
 print('Возраст - ',age)
-print('Будет когда-то - ',new_age)
+print('Новый возраст - ',new_age)
 print('Я, студент',is_student)
