e6ccb21bf4859e038cdde4c1043203d1dec8208b
Khristianna-Vov <vkhristianna@gmail.com>
2024-08-03 20:32:29 +0500

    Add files via upload

---
+++ module_3_5.py
@@ -0,0 +1,17 @@
+def get_multiplied_digits(param):
+    pass
+
+
+def get_multiplied_digits(number):
+    str_number = str(number)
+    first = int(str_number[0])
+    if len(str_number) > 1:
+        return first * get_multiplied_digits(int(str_number[1:]))
+    else:
+        return first
+
+
+# get_multiplied_digits(40203) -> 4 * get_multiplied_digits(203) -> 4 * 2 * get_multiplied_digits(3) -> 4 * 2 * 3)
+
+result = get_multiplied_digits(40203)
+print(result)
