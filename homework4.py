09cb5a6c8bbfe54269caa158a6fd7ae5273bd702
Khristianna-Vov <vkhristianna@gmail.com>
2024-09-06 05:50:36 +0500

    Delete homework4.py

--- homework4.py
+++
@@ -1,7 +0,0 @@
-my_string = input('Укажите Ф.И.О. полностью: ')
-print('Количество символов: ',len(my_string))
-print(my_string.upper()) #Выведите строку my_string в верхнем регистре.
-print(my_string.lower()) # Выведите строку my_string в нижнем регистре.
-print(my_string.replace(' ','')) # Измените строку my_string, удалив все пробелы.
-print(my_string[0])# Выведите первый символ строки my_string.
-print(my_string[-1])# Выведите последний символ строки my_string.
