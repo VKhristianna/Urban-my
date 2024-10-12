8ab800b7a424ccf53126d23475bda18e84d6e145
Khristianna-Vov <vkhristianna@gmail.com>
2024-10-13 04:18:42 +0500

    Delete homework3.py

--- homework3.py
+++
@@ -1,8 +0,0 @@
-name = 'Христианна'
-age = 56
-new_age = age + 5
-is_student = (name == 'Христианна')
-print('Имя -',name)
-print('Возраст - ',age)
-print('Новый возраст - ',new_age)
-print('Я, студент',is_student)
