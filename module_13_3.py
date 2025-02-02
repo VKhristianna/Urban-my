3acb8b1a16d263f443ddeb7dbad2da71d0b431e0
Khristianna-Vov <vkhristianna@gmail.com>
2025-02-02 07:15:47 +0500

    Add files via upload

---
+++ module_13_3.py
@@ -0,0 +1,29 @@
+"""Домашнее задание по теме "Методы отправки сообщений"."""
+#  Цель: написать простейшего телеграм-бота, используя асинхронные функции.
+
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
+
+api = '7676529448:AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
+bot = Bot(token=api)
+
+dp = Dispatcher()
+
+
+@dp.message(lambda message: message.text == 'Привет')
+async def all_massages(message: types.Message):
+    await message.answer('Введите команду /start, чтобы начать общение.')
+
+
+@dp.message(Command("start"))
+async def start(message: types.Message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
+
+# Запуск процесса
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
