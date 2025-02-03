fb436cc1ff3428738831e5ceb64ee75dea95bd77
Khristianna-Vov <vkhristianna@gmail.com>
2025-02-04 03:29:46 +0500

    module_13_4.py

--- module_13_4.py
+++ module_13_4.py
@@ -8,7 +8,7 @@ from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.context import FSMContext
 from aiogram.filters import Command
 
-api = '7676529448:AAEqQk_kFITApUCC0U2pjNq_rEG3gVBkxo4'
+api = '7676529448:AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
 bot = Bot(token=api)
 
 dp = Dispatcher()
@@ -33,14 +33,16 @@ async def set_age(message: types.Message, state: FSMContext):
 # Реагирует на переданное состояние UserState.age
 @dp.message(UserState.age)
 async def set_growth(message: types.Message, state: FSMContext):
-    await state.update_data(age=int(message.text))
+    await state.update_data(age=int(message.text))      # Обновление данных в состоянии age на message.text
+                                                        # (написанное пользователем сообщение)
     await message.answer('Введите свой рост:')
     await state.set_state(UserState.growth)     # Ожидание ввода роста в атрибут UserState.growth
 
 # Реагирует на переданное состояние UserState.growth
 @dp.message(UserState.growth)
 async def set_weight(message: types.Message, state: FSMContext):
-    await state.update_data(growth=int(message.text))
+    await state.update_data(growth=int(message.text))   # Обновление данных в состоянии growth на message.text
+                                                        # (написанное пользователем сообщение)
     await message.answer('Введите свой вес:')
     await state.set_state(UserState.weight)     # Ожидание ввода веса в атрибут UserState.weight
 
@@ -49,7 +51,7 @@ async def set_weight(message: types.Message, state: FSMContext):
 async def send_calories(message: types.Message, state: FSMContext):
     await state.update_data(weight=int(message.text))   # Обновление данных в состоянии weight на message.text
                                                         # (написанное пользователем сообщение)
-    data = await state.get_data()       # Переменная data запоминает все введённые состояния
+    data = await state.get_data()               # Переменная data запоминает все введённые состояния
 
 # Данные для формулы из ранее объявленной переменной data
     age = data['age']
@@ -71,4 +73,6 @@ async def main():
 
 if __name__ == '__main__':
     asyncio.run(main())
+
+
 
